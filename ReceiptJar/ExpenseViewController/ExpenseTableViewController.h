acf2984fcaed06bfa4fdca17b6f4d25a995847cd
Lei Zhao <lzhao@itree.com.au>
2015-03-19 17:07:59 +1100

    .

---
+++ ReceiptJar/ExpenseViewController/ExpenseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExpenseTableViewController.h
+//  ReceiptJar
+//
+//  Created by Lei Zhao on 19/03/2015.
+//  Copyright (c) 2015 funkytonic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpenseTableViewController : UITableViewController
+
+@end
